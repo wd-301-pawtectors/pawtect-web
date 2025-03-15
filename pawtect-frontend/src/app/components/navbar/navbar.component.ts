81f35cfdf77c80edcd59bdea648f4f870e5d0f67
Marcel Angelo Navarro <marcelxangelo@gmail.com>
2025-03-15 23:34:28 +0800

    feat(navbar): add Google Fonts and Font Awesome via CDN, create mobile navbar
    - Added Google Fonts (Poppins and Figtree) and Font Awesome to index.html via CDN
    - Created responsive navbar for mobile viewport
    - Updated HTML, SCSS, and TypeScript files for navbar component

--- pawtect-frontend/src/app/components/navbar/navbar.component.ts
+++ pawtect-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
